038fffc26539137493932fdd517c759fbaaed558
ADB-stark <115967404+ADB-stark@users.noreply.github.com>
2023-02-05 19:46:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ import { useState } from 'react';
 const Home = () => {
   
   const [userInput, setUserInput] = useState('');
+  
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+    setIsGenerating(true);
+
+    console.log("Calling OpenAI...")
+    const response = await fetch('/api/generate', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ userInput }),
+    });
+
+    const data = await response.json();
+    const { output } = data;
+    console.log("OpenAI replied...", output.text)
+
+    setApiOutput(`${output.text}`);
+    setIsGenerating(false);
+  }
+  
   const onUserChangedText = (event) => {
     console.log(event.target.value);
     setUserInput(event.target.value);
@@ -27,14 +51,39 @@ const Home = () => {
           value={userInput}
           onChange={onUserChangedText}
         />
-        {/* New code I added here */}
-        <div className="prompt-buttons">
-          <a className="generate-button" onClick={null}>
-            <div className="generate">
-              <p>Generate</p>
+//         <div className="prompt-buttons">
+//           <a className="generate-button" onClick={callGenerateEndpoint}>
+//             <div className="generate">
+//               <p>Generate</p>
+//             </div>
+//           </a>
+//         </div>
+            
+            <div className="prompt-buttons">
+              <a
+                className={isGenerating ? 'generate-button loading' : 'generate-button'}
+                onClick={callGenerateEndpoint}
+              >
+                <div className="generate">
+                {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+                </div>
+              </a>
             </div>
-          </a>
+            
+        {/* New code I added here */}
+        {apiOutput && (
+        <div className="output">
+          <div className="output-header-container">
+            <div className="output-header">
+              <h3>Output</h3>
+            </div>
+          </div>
+          <div className="output-content">
+            <p>{apiOutput}</p>
+          </div>
         </div>
+      )}
+      </div>
       </div>
       </div>
       <div className="badge-container grow">
