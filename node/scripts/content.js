b17e544789c02043857899efcde0b7d13f562a92
ADB-stark <115967404+ADB-stark@users.noreply.github.com>
2023-02-06 15:10:25 +0530

    Create content.js

---
+++ node/scripts/content.js
@@ -0,0 +1,31 @@
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.message === 'inject') {
+    const { content } = request;
+
+    console.log(content);
+
+    sendResponse({ status: 'success' });
+  }
+});
+
+// Declare new function
+const insert = (content) => {}
+
+chrome.runtime.onMessage.addListener(
+  // This is the message listener
+  (request, sender, sendResponse) => {
+    if (request.message === 'inject') {
+      const { content } = request;
+			
+      // Call this insert function
+      const result = insert(content);
+			
+      // If something went wrong, send a failed status
+      if (!result) {
+        sendResponse({ status: 'failed' });
+      }
+
+      sendResponse({ status: 'success' });
+    }
+  }
+);
