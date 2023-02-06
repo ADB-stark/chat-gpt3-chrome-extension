4ba52c8bb0f970632c6ddd0b47048ad912091527
ADB-stark <115967404+ADB-stark@users.noreply.github.com>
2023-02-06 15:17:56 +0530

    Update content.js

--- node/scripts/content.js
+++ node/scripts/content.js
@@ -9,7 +9,52 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 });
 
 // Declare new function
-const insert = (content) => {}
+// const insert = (content) => {}
+const insert = (content) => {
+  // Find Calmly editor input section
+
+  // Grab the first p tag so we can replace it with our injection
+
+  // Split content by \n
+
+  // Wrap in p tags
+
+  // Insert into HTML one at a time
+
+  // On success return true
+  return true;
+};
+
+// Find Calmly editor input section
+const elements = document.getElementsByClassName('droid');
+
+if (elements.length === 0) {
+  return;
+}
+
+const element = elements[0];
+
+// Grab the first p tag so we can replace it with our injection
+const pToRemove = element.childNodes[0];
+pToRemove.remove();
+
+// Split content by \n
+const splitContent = content.split('\n');
+
+// Wrap in p tags
+splitContent.forEach((content) => {
+  const p = document.createElement('p');
+
+  if (content === '') {
+    const br = document.createElement('br');
+    p.appendChild(br);
+  } else {
+    p.textContent = content;
+  }
+
+  // Insert into HTML one at a time
+  element.appendChild(p);
+});
 
 chrome.runtime.onMessage.addListener(
   // This is the message listener
